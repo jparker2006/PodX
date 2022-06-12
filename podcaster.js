98b61c139166062f5f6bfc73aa097fb9eecaaa62
jparker2006 <jakeparkeremail@gmail.com>
2022-06-11 23:29:37 -0700

    searchs, lists, screens

--- podcaster.js
+++ podcaster.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/*
+screen states:
+- screen doesnt matter: -1,
+- main frame: 0,
+- bug frame: 1
+*/
+
+var g_objUserData = {};
+g_objUserData.state = -1;
+
 onload = () => {
     Header();
     ControlBar();
@@ -13,10 +23,10 @@ const Header = () => {
     sPage += "<div class='g_HeaderName' onClick='MainFrame()'>Podcaster</div>";
     sPage += "<div class='g_HeaderLink' style='width: 116px; left: 168px;'>Subscriptions</div>";
     sPage += "<div class='g_HeaderLink' style='width: 61px; left: 298px;'>History</div>";
-    sPage += "<div class='g_HeaderLink' style='width: 98px; left: 373px;'>Bug Report</div>";
+    sPage += "<div class='g_HeaderLink' style='width: 98px; left: 373px;' onClick='BugFrame()'>Bug Report</div>";
     sPage += "<div class='g_HeaderLink' style='width: 72px; left: 485px;'>Account</div>";
     sPage += "<div class='g_HeaderLink' style='width: 26px; left: 571px;'>";
-    sPage += "<a href='https://github.com/jparker2006/Podcasts' class='g_headerLinkText'>Git</a>";
+    sPage += "<a href='https://github.com/jparker2006/Podcasts' class='g_headerLinkText' target='_blank'>Git</a>";
     sPage += "</div>";
     sPage += "</div>";
     sPage += "</div>";
@@ -43,55 +53,176 @@ const ControlBar = () => {
 }
 
 const MainFrame = () => {
+    if (0 == g_objUserData.state) return;
     let sPage = "";
-    sPage += "<div>";
-
-    sPage += "<div id='podcastsDisplay'>";
+    sPage += "<div class='leftContainer'>";
+    sPage += "<div class='innerLeft'>";
+    sPage += "<span style='font-size: 20px;'>Search for podcasts:</span><br>";
+    sPage += "<input style='width: 90%;' onKeyUp='checkTypingDelay(this.value)' placeholder='Enter Search' />";
     sPage += "</div>";
-
+    sPage += "</div>";
+    sPage += "<div id='podcastsDisplay' class='podcastsDisplay'>";
     sPage += "</div>";
     document.getElementById('Main').innerHTML = sPage;
     pullPodcasts();
+    g_objUserData.state = 0;
 }
 
-const pullPodcasts = (sSearch = 1) => {
-    postFileFromServer("main.php", "pullPodcasts=" + encodeURIComponent(sSearch), pullPodcastsCallback);
-    function pullPodcastsCallback(data) {
-        displayPodcasts(data);
+const BugFrame = () => {
+    if (1 == g_objUserData.state) return;
+    let sPage = "";
+    sPage += "<div class='leftContainer'>";
+    sPage += "<div class='innerLeft'>";
+    sPage += "<span style='font-size: 20px; font-weight: 500;'>Current Bugs:</span><br>";
+    sPage += "<div id='allBugs'></div>";
+    sPage += "</div>";
+    sPage += "</div>";
+    sPage += "<div id='podcastsDisplay' class='podcastsDisplay'>";
+    sPage += "<textarea id='bugData' class='bugInsertTextBox' maxlength=300 placeholder='What are you having problems with?'></textarea>";
+    sPage += "<button class='bugInsertButton' onClick='submitBug()'>Submit!</button>";
+    sPage += "<span id='feedback'></span>";
+    sPage += "</div>";
+    document.getElementById('Main').innerHTML = sPage;
+    showMeTheBugs();
+    g_objUserData.state = 1;
+}
+
+const PodcastOverviewFrame = (xmlDoc) => {
+    let sTitle = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+    let sDescription = xmlDoc.getElementsByTagName("description")[0].childNodes[0].nodeValue;
+    let sImgURL = xmlDoc.getElementsByTagName("url")[0].childNodes[0].nodeValue;
+    let aItems = xmlDoc.getElementsByTagName("item");
+    let sPage = "";
+    sPage += "<div class='leftContainer'>";
+    sPage += "<div class='innerLeft'>";
+    sPage += "<span style='font-size: 17px; font-weight: 500;'>" + sTitle + "</span><br>";
+    sPage += "<img src='" + sImgURL + "' style='width: 120px; height: 120px;'</img><br>";
+    sPage += "<input id='episodeSearch' style='width: 200px;' placeholder='Search for an Episode' /><br>";
+    sPage += "<span style='font-size: 12px;'>" + sDescription + "</span><br>";
+    sPage += "</div>";
+    sPage += "</div>";
+    sPage += "<div class='podcastsDisplay'>";
+    sPage += "<span style='font-size: 20px;'>Episodes:</span><br>";
+    sPage += "<div id='episodesDisplay'></div>";
+    sPage += "</div>";
+    document.getElementById('Main').innerHTML = sPage;
+    g_objUserData.state = -1;
+    document.getElementById('episodeSearch').onkeyup = function() { searchThroughEpisodes(aItems) };
+    searchThroughEpisodes(aItems);
+}
+
+const searchThroughEpisodes = (aItems) => {
+    let sPage = "";
+    let sQuery = document.getElementById('episodeSearch').value;
+    for (let i=0; i<aItems.length; i++) {
+        let sCurrTitle = aItems[i].firstElementChild.textContent;
+        if (-1 == sCurrTitle.toLowerCase().indexOf(sQuery.toLowerCase()))
+            continue;
+        sPage += "<div id='"+i+"' class='episodeTitleDisplay'>";
+        sPage += sCurrTitle;
+        sPage += "</div>";
+    }
+    document.getElementById('episodesDisplay').innerHTML = sPage;
+    for (let i=0; i<aItems.length; i++) {
+        document.getElementById(i).onclick = function() { playPodcast(aItems[i]) };
     }
 }
 
-const displayPodcasts = (jsonPodcasts) => {
-    if (!jsonPodcasts) {
-        document.getElementById('podcastsDisplay').innerHTML = "no podcasts within your search<br>insert this podcast?";
+const playPodcast = (objEpisode) => {
+    console.log(objEpisode);
+}
+
+const showMeTheBugs = () => {
+    postFileFromServer("podcaster.php", "pullBugs=" + encodeURIComponent(1), showMeTheBugsCallback);
+    function showMeTheBugsCallback(data) {
+        if (!data) {
+            let sPage = "No current bugs, find some!<br>";
+            sPage += "<div class='fixBugsContainer'>";
+            sPage += "<a href='https://github.com/jparker2006/Podcasts' class='g_headerLinkText fixBugsLink' target='_blank'>Improve the platform</a>";
+            sPage += "</div>";
+            document.getElementById('allBugs').innerHTML = sPage;
+            return;
+        }
+        let objBugs = JSON.parse(data);
+        let sPage = "";
+        for (let i=0; i<objBugs.length; i++) {
+            sPage += "<div style='margin-bottom: 5px;'>" + objBugs[i].bug + "</div>";
+        }
+        sPage += "<div class='fixBugsContainer'>";
+        sPage += "<a href='https://github.com/jparker2006/Podcasts' class='g_headerLinkText fixBugsLink' target='_blank'>Help fix some bugs</a>";
+        sPage += "</div>";
+        document.getElementById('allBugs').innerHTML = sPage;
+    }
+}
+
+const submitBug = () => {
+    let sBug = document.getElementById('bugData').value;
+    if (!sBug) {
+        document.getElementById('feedback').innerHTML = "<br>Bugs must have content";
         return;
     }
-    let objPodcasts = JSON.parse(jsonPodcasts);
-    let sPage = "";
-    for (let i=0; i<objPodcasts.length; i++) {
-        sPage += objPodcasts[i].title + "<br>";
+    postFileFromServer("podcaster.php", "insertBug=" + encodeURIComponent(sBug), insertBugCallback);
+    function insertBugCallback(data) {
+        document.getElementById('bugData').value = "";
+        if (data) {
+            document.getElementById('feedback').innerHTML = "<br>Thank you!<br>This will be resolved as soon as possible";
+            return;
+        }
+        else
+            document.getElementById('feedback').innerHTML = "<br>Possible network error";
     }
-    document.getElementById('podcastsDisplay').innerHTML = sPage;
+}
+
+const checkTypingDelay = (sSearch) => {
+    g_objUserData.ajaxing = sSearch.length;
+    setTimeout(function() {
+        if (sSearch.length == g_objUserData.ajaxing)
+            pullPodcasts(sSearch);
+    }, 500);
+}
+
+const parseRSSLink = (sLink, functionCall) => {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (4 == this.readyState && 200 == this.status)
+            functionCall(this.responseXML);
+    };
+    xmlhttp.open("GET", sLink, true);
+    xmlhttp.send();
+}
+
+const pullPodcasts = (sSearch) => {
+    let objPullData = {};
+    if (!sSearch) {
+        if (15 <= document.getElementById('podcastsDisplay').innerHTML.split("podcastResult").length) // so spamming del key doesnt overload the server
+            return;
+        objPullData.bSearching = false;
+    }
+    else {
+        objPullData.bSearching = true;
+        objPullData.search = sSearch;
+    }
+    let jsonPullData = JSON.stringify(objPullData);
+    postFileFromServer("podcaster.php", "pullPodcasts=" + encodeURIComponent(jsonPullData), pullPodcastsCallback);
+    function pullPodcastsCallback(data) {
+        if (!data) {
+            document.getElementById('podcastsDisplay').innerHTML = "<span style='font-size: 20px;'>no podcasts within your search<br>insert a podcast?</span>";
+            return;
+        }
+        let objPodcasts = JSON.parse(data);
+        let sPage = "<span style='font-size: 20px;'>Podcasts Within Search:</span><br>";
+        for (let i=0; i<objPodcasts.length; i++) {
+            sPage += "<div class='podcastResult' onClick='parseRSSLink(\"" + objPodcasts[i].link + "\", PodcastOverviewFrame)'>"; // fix this out of quote hell
+            sPage += objPodcasts[i].title;
+            sPage += "</div>";
+        }
+        document.getElementById('podcastsDisplay').innerHTML = sPage;
+    }
 }
 
 const dragAudio = () => {
     alert();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const HashThis = (sText, nRounds) => {
     for (let x = 0; x < nRounds; x++) {
---
+++ podcaster.php
@@ -0,0 +1,119 @@
+<?php
+
+if (isset($_POST['pullPodcasts']))
+    $jsonPullData = $_POST['pullPodcasts'];
+else if (isset($_POST['insertBug']))
+    $sBug = $_POST['insertBug'];
+else if (isset($_POST['pullBugs']))
+    $bPullTheBugs = $_POST['pullBugs'];
+
+
+if ($jsonPullData)
+    $sFeedback = pullPodcasts ($jsonPullData);
+else if ($sBug)
+    $sFeedback = insertBug ($sBug);
+else if ($bPullTheBugs)
+    $sFeedback = pullBugs ();
+
+echo $sFeedback;
+
+function pullPodcasts ($jsonPullData) {
+    $objPullData = json_decode ($jsonPullData);
+
+    $dbhost = 'localhost';
+    $dbuser = 'podcasts_site';
+    $dbpass = '0ShF3HctflFXwkhQSYte';
+    $db = "podcaster";
+    $dbconnect = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    $aPodcasts = [];
+
+    if (!$objPullData->bSearching) {
+        $sSQL = "SELECT COUNT(*) FROM Podcasts";
+        $tResult = QueryDB($sSQL);
+        $nPodcasts = $tResult->fetch_assoc()["COUNT(*)"];
+        $aRandomPulls = [random_int(1, $nPodcasts)];
+        for ($x=1; $x<15; $x++) {
+            $randCurr = random_int(1, $nPodcasts);
+            while (in_array($randCurr, $aRandomPulls)) {
+                $randCurr = random_int(1, $nPodcasts);
+            }
+            $aRandomPulls[$x] = $randCurr;
+        }
+
+        for ($i=0; $i<15; $i++) {
+            $sSQL = "SELECT * FROM Podcasts WHERE id=" . $aRandomPulls[$i];
+            $tResult = QueryDB($sSQL);
+            $row = $tResult->fetch_assoc();
+            $aPodcasts[$i] = new stdClass();
+            $aPodcasts[$i]->id = $row["id"];
+            $aPodcasts[$i]->title = $row["title"];
+            $aPodcasts[$i]->description = $row["description"];
+            $aPodcasts[$i]->link = $row["link"];
+            $aPodcasts[$i]->created = $row["created"];
+        }
+    }
+    else {
+        $objPullData->search = "%" . $objPullData->search . "%";
+        $stmt = $dbconnect->prepare("SELECT * FROM Podcasts WHERE title LIKE ? OR description LIKE ? ORDER BY id DESC LIMIT 15");
+        $stmt->bind_param("ss", $objPullData->search, $objPullData->search);
+        $stmt->execute();
+        $tResult = $stmt->get_result();
+        $stmt->close();
+        $dbconnect->close();
+        for ($i=0; $i<$tResult->num_rows; $i++) {
+            $row = $tResult->fetch_assoc();
+            $aPodcasts[$i] = new stdClass();
+            $aPodcasts[$i]->id = $row["id"];
+            $aPodcasts[$i]->title = $row["title"];
+            $aPodcasts[$i]->description = $row["description"];
+            $aPodcasts[$i]->link = $row["link"];
+            $aPodcasts[$i]->created = $row["created"];
+        }
+    }
+    if (0 == sizeof($aPodcasts))
+        return null;
+    return json_encode ($aPodcasts);
+}
+
+function insertBug ($sBug) {
+    $dbhost = 'localhost';
+    $dbuser = 'podcasts_site';
+    $dbpass = '0ShF3HctflFXwkhQSYte';
+    $db = "podcaster";
+    $dbconnect = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+    $stmt = $dbconnect->prepare("INSERT INTO Bugs (bug) VALUES (?)");
+    $stmt->bind_param("s", $sBug);
+    $bStatus = $stmt->execute();
+    $stmt->close();
+
+    return $bStatus;
+}
+
+function pullBugs () {
+    $sSQL = "SELECT * FROM Bugs LIMIT 15";
+    $tResult = QueryDB($sSQL);
+    $aBugs = [];
+    for ($i=0; $i<$tResult->num_rows; $i++) {
+        $row = $tResult->fetch_assoc();
+        $aBugs[$i] = new stdClass();
+        $aBugs[$i]->bug = $row["bug"];
+        $aBugs[$i]->created = $row["created"];
+    }
+    if (0 == sizeof($aBugs))
+        return null;
+    return json_encode ($aBugs);
+}
+
+function QueryDB ($sSQL) {
+    $dbhost = 'localhost';
+    $dbuser = 'podcasts_site';
+    $dbpass = '0ShF3HctflFXwkhQSYte';
+    $db = "podcaster";
+    $dbconnect = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    $Result = $dbconnect->query($sSQL);
+    $dbconnect->close();
+    return $Result;
+}
+
+?>
